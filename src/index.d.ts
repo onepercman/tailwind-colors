d5041ac62947426ad427fab4f7a6fda9a420cf6d
onepercman <onepercman@gmail.com>
2024-05-28 16:10:11 +0700

    🐞 fix colorize type

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,7 @@ import plugin from 'tailwindcss/plugin.js'
 import { Color, Scheme, TailwindColorsConfig } from './types'
 
 declare const schemes: (config?: TailwindColorsConfig) => ReturnType<typeof plugin>
-declare const colorize: <C extends Color>(color: C, key: keyof C) => Color
+declare const colorize: <C extends Color>(color: C, key?: keyof C) => Color
 
 export type { Color, Scheme, TailwindColorsConfig }
 
